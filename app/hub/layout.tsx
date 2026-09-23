ebb5c3f41c753e78e8af932fae3d16fbba30651c
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2026-09-23 21:08:20 +0200

    style(hub): load thread v2 styles

--- app/hub/layout.tsx
+++ app/hub/layout.tsx
@@ -24,6 +24,7 @@ import "./prospects-v3.css";
 import "./prospects-more-compact.css";
 import "./prospects-buttons-compact.css";
 import "./prospects-top10.css";
+import "./thread-v2.css";
 
 export const metadata: Metadata = {
   title: "Staark Hub",
