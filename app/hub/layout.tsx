ad28c6583a50fb003265e8ee4be84b0a69199e18
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2026-09-24 16:32:04 +0200

    style(hub): load project WordPress styles

--- app/hub/layout.tsx
+++ app/hub/layout.tsx
@@ -4,6 +4,7 @@ import "./overview-command-center.css";
 import "./offer-project-link.css";
 import "./project-demo-management.css";
 import "./project-detail-polish.css";
+import "./project-wordpress.css";
 import "./projects-v2.css";
 import "./client-workspace-polish.css";
 import "./contacts-workspace.css";
